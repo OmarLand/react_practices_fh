11752a4de36b26a1fd21df3219da6718b080b991
OmarjLand <omar.land2010@gmail.com>
2024-01-14 23:13:26 +0100

    Fixed a bit the structure

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export const GifExpertApp = () => {
   return (
     <>
+        {/* Titulo de la explicación */}
         <h1>Gif Expert App</h1>
+
+
+        {/* Input */}
+
+
+        {/* Listado de los Gif! */}
+            {/* Gif Item */}
     </>
   )
 }
