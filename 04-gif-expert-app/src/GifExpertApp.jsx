18a2f8229bc25d7cb797867aceaf26f603ee8531
OmarjLand <omar.land2010@gmail.com>
2024-01-15 23:13:36 +0100

    feat: Trying the hook useState

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'Dragon Ball','Death Note' ])
+
+  // console.log( categories );
+
+  const onAddCategory = () => {
+    setCategories([...categories, "tekken"]);
+    // setCategories( cat => [ ...cat, "tekken" ] );
+
+  }
+
   return (
     <>
         {/* Titulo de la explicación */}
@@ -11,6 +22,14 @@ export const GifExpertApp = () => {
 
 
         {/* Listado de los Gif! */}
+
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+          { categories.map( categorie => {
+            return <li key={categorie}>{categorie}</li>;
+          })}
+          {/* <li>abc</li> */}
+        </ol>
             {/* Gif Item */}
     </>
   )
