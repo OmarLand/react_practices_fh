6fc65381ca5457071d0968b51176db682744d39f
OmarjLand <omar.land2010@gmail.com>
2026-05-02 14:52:45 +0200

    Aprendiendo concepto de useReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Padre } from './07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer'
+
 
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
